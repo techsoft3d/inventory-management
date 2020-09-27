2da9488eef91eddfa4bf4bb60b61caf00a957f08
Ryan Persson <ryan.persson@techsoft3d.com>
2020-09-27 14:52:53 -0700

    Fixed loading models from thumbnails

--- src/js/app.js
+++ src/js/app.js
@@ -21,11 +21,16 @@ class main {
         this._viewer.setCallbacks({
             modelStructureReady: () => {
                 // Background color for viewers
-                this._viewer.view.setBackgroundColor(new Communicator.Color(100, 150, 200), new Communicator.Color(222, 222, 222));
+                this._viewer.view.setBackgroundColor(
+                    new Communicator.Color(100, 150, 200),
+                     new Communicator.Color(222, 222, 222)
+                     );
                 // Additional viewer options
                 this._viewer.view.getAxisTriad().enable();
                 this._viewer.view.getNavCube().enable();
-                this._viewer.view.getNavCube().setAnchor(Communicator.OverlayAnchor.LowerRightCorner);
+                this._viewer.view
+                    .getNavCube()
+                    .setAnchor(Communicator.OverlayAnchor.LowerRightCorner);
                 let loadButton = document.getElementById("open-model-button");
                 loadButton.disabled = false;
             },
@@ -58,7 +63,22 @@ class main {
             }
         }); // End Callbacks
         this.setEventListeners();
+        this.updateModelList();
     } // End app Constructor
+      //Function to preload model files to be accessed through thumbnails
+  updateModelList() {
+    const thumbnailElements = document.getElementsByClassName("model-thumb");
+    for (let thumbnail of thumbnailElements) {
+      let thumbnailElement = thumbnail;
+      thumbnailElement.onclick = e => {
+        e.preventDefault();
+        let elem = e.currentTarget;
+        let modelToLoad = elem.getAttribute("model");
+        // Load the model into the scene when clicked
+        this.loadModel(modelToLoad);
+      };
+    }
+  }
     // Function to load models and query any associated application data
     loadModel(modelName) {
         this._viewer.model.clear()
@@ -86,23 +106,29 @@ class main {
                             }
                             ;
                             // Display the total cost of the assembly
-                            document.getElementById("inv-total-cost").innerHTML = `$ ${totalCost.toFixed(2)}`;
+                            document.getElementById("inv-total-cost")
+                                .innerHTML = `$ ${totalCost.toFixed(2)}`;
                             this._displayFilter.captureNativeColors(this._modelData);
                             this._displayFilter.gatherFilteredNodes(this._modelData);
                             this._displayFilter.updateColorGradients(this._modelData);
                             this._displayFilter.setRenderingSelection();
                         });
-                    }
-                    else {
+                    } else {
                         alert("No JSON data for this Model was found.");
                     }
                 });
                 // Get and set the rest of the model level info
                 const modelRoot = this._viewer.model.getNodeChildren(modelNodeId)[0];
-                const modelFileName = this._viewer.model.getModelFileNameFromNode(modelRoot);
-                const modelFileFormat = this._viewer.model.getModelFileTypeFromNode(modelRoot);
-                document.getElementById("model-file-name").innerHTML = modelFileName || "N/A";
-                document.getElementById("model-file-type").innerHTML = Communicator.FileType[modelFileFormat] || "N/A";
+                const modelFileName = this._viewer.model.getModelFileNameFromNode(
+                    modelRoot
+                  );
+                  const modelFileFormat = this._viewer.model.getModelFileTypeFromNode(
+                    modelRoot
+                  );
+                  document.getElementById("model-file-name").innerHTML =
+                    modelFileName || "N/A";
+                  document.getElementById("model-file-type").innerHTML =
+                    Communicator.FileType[modelFileFormat] || "N/A";
             });
         });
     }
@@ -127,7 +153,7 @@ class main {
         };
         document.getElementById("open-model-button").onclick = () => {
             // Proxy to override the default behavior of file input type
-            document.getElementById('file-input').click();
+            document.getElementById("file-input").click();
         };
         document.getElementsByName("displaymode").forEach((element) => {
             let inputElement = element;
@@ -144,15 +170,16 @@ class main {
                 this._displayFilter.setRenderingSelection();
             };
         });
-        let compButtons = document.getElementById("companyFilter").getElementsByClassName("companyFilterButton");
+        let compButtons = document
+            .getElementById("companyFilter")
+            .getElementsByClassName("companyFilterButton");
         for (let element of compButtons) {
             let htmlelement = element;
             htmlelement.onclick = () => {
                 if (htmlelement.classList.contains("selected")) {
                     htmlelement.classList.remove("selected");
                     this._displayFilter.removeCompany(htmlelement.alt);
-                }
-                else {
+                } else {
                     htmlelement.classList.add("selected");
                     this._displayFilter.addCompany(htmlelement.alt);
                 }
@@ -164,41 +191,28 @@ class main {
             // Once a file has been selected by the user, use the file information to 
             // gather the associated relevant data like thumbnails
             let fileChoice = e.target.value;
-            let filename = fileChoice.replace(/^.*[\\\/]/, '');
-            let modelThumbnail = document.createElement('a');
+            let filename = fileChoice.replace(/^.*[\\\/]/, "");
+            let modelThumbnail = document.createElement("a");
             let modelname = filename.split(".", 1)[0];
             modelThumbnail.id = modelname;
             modelThumbnail.href = "";
             modelThumbnail.className = "model-thumb";
             modelThumbnail.setAttribute("model", modelname);
-            let imgPath = "/data/thumbnails/" + modelname + ".png";
+            let imgPath = "./data/thumbnails/" + modelname + ".png";
             // Check to see if the selected model has a corresponding thumbnail made
-            fetch(imgPath)
-                .then((resp) => {
+            fetch(imgPath).then(resp => {
                 if (resp.ok) {
-                    let modelImg = document.createElement('img');
+                    let modelImg = document.createElement("img");
                     modelImg.src = imgPath;
                     modelThumbnail.appendChild(modelImg);
-                }
-                else {
+                } else {
                     modelThumbnail.innerHTML = modelname;
                     console.log("No Image for this Model was found.");
                 }
             });
             document.getElementById("models-scroller").appendChild(modelThumbnail);
             // Now update the event callbacks for the thumbnails
-            const thumbnailElements = document.getElementsByClassName("model-thumb");
-            for (let thumbnail of thumbnailElements) {
-                let thumbnailElement = thumbnail;
-                thumbnailElement.onclick = (e) => {
-                    e.preventDefault();
-                    let elem = e.currentTarget;
-                    let modelToLoad = elem.getAttribute("model");
-                    // Load the model into the scene when clicked
-                    this.loadModel(modelToLoad);
-                };
-            }
-            ;
+            this.updateModelList();
         };
     } // End setting event handlers 
 } // End app class 
