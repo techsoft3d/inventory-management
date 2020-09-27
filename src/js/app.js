984244d542ef1a7e38d5bdb658fb04565af977cc
Ryan Persson <ryan.persson@techsoft3d.com>
2020-09-27 16:43:46 -0700

    Added step 5 code

--- src/js/app.js
+++ src/js/app.js
@@ -29,8 +29,74 @@ class main {
                 // If the selection nodeId is found in the application data, populate the inspector fields with application data
             }
         }); // End Callbacks
+        this.setEventListeners();
     } // End main constructor
+    loadModel(modelName) {
+        this._viewer.model.clear()
+            .then(() => {
+            const nodeName = "Model-" + modelName;
+            const modelNodeId = this._viewer.model.createNode(null, nodeName);
+            this._viewer.model.loadSubtreeFromScsFile(modelNodeId, "/data/" + modelName + ".scs")
+          .then(() => {
+            this._viewer.view.fitWorld();
+           // Get and set the rest of the model level info
+           const modelRoot = this._viewer.model.getNodeChildren(modelNodeId)[0];
+           const modelFileName = this._viewer.model.getModelFileNameFromNode(modelRoot);
+           const modelFileFormat = this._viewer.model.getModelFileTypeFromNode(modelRoot);
+           document.getElementById("model-file-name").innerHTML = modelFileName || "N/A";
+           document.getElementById("model-file-type").innerHTML = Communicator.FileType[modelFileFormat] || "N/A";
+          });
+        });
+    }
+    setEventListeners() {
+        document.getElementById("open-model-button").onclick = () => {
+            // Proxy to override the default behavior of file input type
+            document.getElementById('file-input').click();
+        };
+        document.getElementById("file-input").onchange = (e) => {
+            // Once a file has been selected by the user, use the file information to 
+            // gather the associated relevant data like thumbnails
+            let fileChoice = e.target.value;
+            let filename = fileChoice.replace(/^.*[\\\/]/, '');
+            let modelThumbnail = document.createElement('a');
+            let modelname = filename.split(".", 1)[0];
+            modelThumbnail.id = modelname;
+            modelThumbnail.href = "";
+            modelThumbnail.className = "model-thumb";
+            modelThumbnail.setAttribute("model", modelname);
+            let imgPath = "/data/thumbnails/" + modelname + ".png";
+            // Check to see if the selected model has a corresponding thumbnail made
+            fetch(imgPath)
+                .then((resp) => {
+                if (resp.ok) {
+                    let modelImg = document.createElement('img');
+                    modelImg.src = imgPath;
+                    modelThumbnail.appendChild(modelImg);
+                }
+                else {
+                    modelThumbnail.innerHTML = modelname;
+                    console.log("No Image for this Model was found.");
+                }
+            });
+            document.getElementById("models-scroller").appendChild(modelThumbnail);
+            // Now update the event callbacks for the thumbnails
+            const thumbnailElements = document.getElementsByClassName("model-thumb");
+            for (let thumbnail of thumbnailElements) {
+                let thumbnailElement = thumbnail;
+                thumbnailElement.onclick = (e) => {
+                    e.preventDefault();
+                    let elem = e.currentTarget;
+                    let modelToLoad = elem.getAttribute("model");
+                    // Load the model into the scene when clicked
+                    this.loadModel(modelToLoad);
+                };
+            }
+            ;
+        };
+    } // End setting even handlers
 } // End main class
+
+
 
 
 
