ec784a9bb5089fc39232613a447713c665cb5d1b
Chris Pillion <cpillion@techsoft3d.com>
2021-01-31 09:35:38 -0800

    Completed 'Setting Up The Main Viewer' steps

--- src/js/app.js
+++ src/js/app.js
@@ -8,8 +8,45 @@ window.onload = () => {
 
 class main {
   constructor() {
+    // Instantiate the viewer
+    this._viewer = new Communicator.WebViewer({
+      containerId: "viewer",
+      empty: true
+    });
+    this._viewer.start();
+
+    // Set the viewer callback functionality
+    this._viewer.setCallbacks({
+
+      modelStructureReady: () => {
+
+          // Enable load model button
+          let loadButton = document.getElementById("open-model-button");
+          loadButton.disabled = false;
+
+      },
+
+      selectionArray: (selectionEvents) => {
+          // Reset info fields if no selection item was chosen
+          // Otherwise, display node information for the first node in the selection array
+          // If the selection nodeId is found in the application data, populate the inspector fields with application data
+      }, 
+
+      sceneReady: () => {
+
+          // Set Background color for viewers
+          this._viewer.view.setBackgroundColor(new Communicator.Color(0, 153, 220), new Communicator.Color(218, 220, 222));
+
+          // Enable nav cube and axis triad
+          this._viewer.view.getAxisTriad().enable();
+          this._viewer.view.getNavCube().enable();
+          this._viewer.view.getNavCube().setAnchor(Communicator.OverlayAnchor.LowerRightCorner);
+
+      }
+    }); // End Callbacks
+
     this.setEventListeners();
-  } // End app Constructor
+  } // End app constructor
 
   setEventListeners() {
     document.getElementById("psMinSlider").oninput = () => {
@@ -24,15 +61,15 @@ class main {
     document.getElementById("ssMaxSlider").oninput = () => {
 
     };
-    document.getElementById("open-model-button").onclick = () => {
-
-    };
     document.getElementsByName("displaymode").forEach(element => {
 
     });
     document.getElementsByName("gradientmode").forEach(element => {
 
     });
+    document.getElementById("open-model-button").onclick = () => {
+
+    };
     document.getElementById("file-input").onchange = e => {
 
     };
